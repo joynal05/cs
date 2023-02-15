95ce29062b0fa6bd9bd3f947d5f385fe28bb6974
Joynal Abedin <joynal.9933@gmail.com>
2023-02-15 10:28:04 +0600

    dsfsdf

---
+++ active.js
@@ -0,0 +1,16 @@
+(function ($) {
+    "use strict";
+    $(document).ready(function () {
+        $.getJSON("https://raw.githubusercontent.com/joynal9933/cs/main/data.json", function(jsonData) {
+            console.log(jsonData);
+            const container = document.getElementById('data-container');
+            jsonData.data.forEach((item) => {
+                const wrap = $('div.'+item.id).text(" ");
+                const title = $('<h2>').text(item.title);
+                const description = $('<div>').text(item.description);
+                const wrapper = $('<div class="item" id="item-'+item.id+'">').append(title).append(description);
+                $('#data-container').append(wrapper);
+            });
+        });
+    });
+}(jQuery));
