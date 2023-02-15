5bc4acfd742d46b7fe457bd485effa84ff91ab43
Joynal Abedin <joynal.9933@gmail.com>
2023-02-15 10:32:40 +0600

    sdfsd

--- active.js
+++ active.js
@@ -1,13 +1,14 @@
 (function ($) {
     "use strict";
     $(document).ready(function () {
+        var jsonData = "";
         $.getJSON("https://raw.githubusercontent.com/joynal9933/cs/main/data.json", function(jsonData) {
             console.log(jsonData);
             const container = document.getElementById('data-container');
             jsonData.data.forEach((item) => {
                 const wrap = $('div.'+item.id).text(" ");
                 const title = $('<h2>').text(item.title);
-                const description = $('<div>').text(item.description);
+                const description = $('<div>').html(item.description);
                 const wrapper = $('<div class="item" id="item-'+item.id+'">').append(title).append(description);
                 $('#data-container').append(wrapper);
             });
