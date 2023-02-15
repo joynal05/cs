1649e4dbeba9abdbe78b0f6ed795856f54f9502b
Joynal Abedin <joynal.9933@gmail.com>
2023-02-15 12:47:33 +0600

    json update

--- active.js
+++ active.js
@@ -3,7 +3,6 @@
     $(document).ready(function () {
         $.getJSON("https://raw.githubusercontent.com/joynal9933/cs/main/data.json", function(jsonData) {
             console.log(jsonData);
-            const container = document.getElementById('data-container');
             jsonData.data.forEach((item) => {
                 const wrap = $('div.'+item.id).text(" ");
                 const title = $('<h2 class="title">').text(item.id+". "+item.title);
